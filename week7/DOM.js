7bc268edef0bfd6fb80982a70fae7f16ff9d7f02
Kittihengcharoen Chaemprasert <kinnom@Kitticharoens-MacBook-Pro-2.local>
2025-10-16 20:53:56 +0700

    third commit

--- week7/DOM.js
+++ week7/DOM.js
@@ -3,7 +3,7 @@ console.log(window.document);
 console.log(document);
 
 
-//root element node , document element 
+//root element (document element) 
 const htmlElement = document.documentElement
 console.log(htmlElement);
 
@@ -21,21 +21,23 @@ console.log(document.lastElementChild);
 
 console.log(htmlElement.parentElement); // null เพราะไม่มี element ไหนสูงกว่าแล้ว
 console.log(htmlElement.parentNode);// root node 
+console.log(document.firstChild.nextSibling); // html element 
+
+
 
 //get all child node under id soup
 const childElement = document.getElementById('soup')
-const childNodes = childElement.childNodes
+const childNodes = childElement.childNodes //ได้ลูกที่เป็น type อะไรก็ได้
 console.log(childNodes.length);
 
-
+const child = childElement.children //ได้ลูกที่เป็นเฉพาะ element 
 //node type children 
 childNodes.forEach( child => console.log(child))
 
-
-
-
-
-
+// how to get attributes 
+const divElementNode = document.getElementById("soup") // query node นั้นมาก่อนแล้วค่อยเข้าภึง attributes
+const divAttributes = divElementNode.attributes
+console.log(divAttributes.length);
 
 
 
---
+++ week7/main.js
@@ -0,0 +1,19 @@
+//basic DOM
+
+const rootNode = window.document
+console.log(rootNode);
+
+const soupMenu1 = document.querySelector("#soup")
+const firstMenu = soupMenu1.firstElementChild
+console.log(firstMenu);
+
+
+const soupMenu2 = document.getElementById("soup")
+const SpicySoup = soupMenu2.children[0]
+console.log(beefSoup.textContent); //เอาแค่เฉพาะข้อความ
+
+
+
+
+
+// 
